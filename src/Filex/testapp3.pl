ea147eed09d159afe6068c4ca618e94336ab91fd
Fred Mesnard <frederic.mesnard@gmail.com>
2022-05-27 10:21:49 +0400

    Update testapp3.pl

--- src/Filex/testapp3.pl
+++ src/Filex/testapp3.pl
@@ -1,14 +1,24 @@
 
+% app3(Xs, Ys, Zs, Us) iff Us is the result of concatentating the lists Xs, Ys and Zs
+app3(Xs,Ys,Zs,Us) :- app(Xs,Ys,Vs), app(Vs,Zs,Us).
 
-% app3(Xs, Ys, Zs, Us) :- Us is the result of concatentating the lists Xs, Ys and Zs
-app3(Xs, Ys, Zs, Us) :- app(Xs, Ys, Vs), app(Vs, Zs, Us).
+% idem but note that the atoms in the body have been swapped
+app3bis(Xs,Ys,Zs,Us) :- app(Vs,Zs,Us), app(Xs,Ys,Vs).
 
-% another possibility
-app3bis(Xs, Ys, Zs, Us):- app(Vs,Zs,Us),app(Xs,Ys,Vs).
-
-% augmented by the APPEND program
-app([], Ys, Ys).
-app([X | Xs], Ys, [X | Zs]) :- app(Xs, Ys, Zs).
-
+% app(Xs,Ys,Zs) iff Zs is the concatenation of Xs and Ys 
+app([],Ys,Ys).
+app([X|Xs],Ys,[X|Zs]) :- app(Xs,Ys,Zs).
+	
+% rev(Xs,Ys) iff Ys is Xs in the reverse order
 rev([],[]).
-rev([X|Xs],Zs):-rev(Xs,Ys),app(Ys,[X],Zs).
+rev([X|Xs],Zs) :- rev(Xs,Ys), app(Ys,[X],Zs).
+
+
+/*
+% ./cTI Filex/testapp3.pl    
+...
+predicate_term_condition(rev(A,B),A).
+predicate_term_condition(app(A,B,C),C+A).
+predicate_term_condition(app3(A,B,C,D),A*D+A*B).
+predicate_term_condition(app3bis(A,B,C,D),D).
+*/
